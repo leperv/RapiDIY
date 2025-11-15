1ab0967d84991c4d9b28678c46dc9c1df08240b0
Ziv <zivtenen14@gamil.com>
2025-11-15 11:45:25 +0200

    added the function that sends the recipe + quey to the ollama local model, and returns the result. also added .idea to git ignore for now

--- src/Engine/utils/query_sending_utils.py
+++ src/Engine/utils/query_sending_utils.py
@@ -1,3 +1,5 @@
+from ollama import chat
+from ollama import ChatResponse
 from src.Recipes.base_class import Recipe
 from src.Recipes.variables import (cartesian_join_dict,union_change_dict,
                                    non_exist_to_left_dict,cte_merge_dict,cte_pre_agg_dict,magic_dict)
@@ -9,3 +11,10 @@ def appned_query_to_prompt(query, recipe):
         recipe.prompt = updated_recipe_prompt
 
 def send_prompt_to_model(recipe):
+    response: ChatResponse = chat(model='gemma3:12b', messages=[
+        {
+            'role': 'user',
+            'content': f'{recipe.prompt}',
+        },
+    ])
+    return response.message.content
